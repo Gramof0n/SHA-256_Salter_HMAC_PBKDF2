33456fb200fbc87c8e7dae37963afdfe71e5bd29
Gramofon <milovanovic.nikolafit@gmail.com>
2021-11-28 12:10:19 +0100

    add: display functions, salt generating

--- src/core/PasswordSalter.java
+++ src/core/PasswordSalter.java
@@ -10,21 +10,40 @@ public class PasswordSalter {
 
 	private final Random r = new Random();
 	private MessageDigest md;
+	private byte[] salt;
 
 	public PasswordSalter() {
 
 	}
 
-	public byte[] salt(String pass) throws NoSuchAlgorithmException {
-		md = MessageDigest.getInstance("SHA-256");
-		byte[] salt = new byte[32];
-
+	public void generateRandomSalt() {
+		salt = new byte[32];
 		r.nextBytes(salt);
+	}
+
+	public void generateSaltFromString(String saltString) {
+		salt = saltString.getBytes();
+	}
+
+	public byte[] salt(String pass) throws NoSuchAlgorithmException {
+		if (salt == null) {
+			System.err.println("No salt generated");
+			return null;
+		}
+		md = MessageDigest.getInstance("SHA-256");
 
 		md.update(Formating.concatenateByteArrays(pass.getBytes(), salt));
 		byte[] digest = md.digest();
 
 		return digest;
+	}
+
+	public String saltAsHex() {
+		if (salt == null) {
+			System.err.println("No salt generated");
+			return "";
+		}
+		return Formating.byte2HexStr(salt);
 	}
 
 }
